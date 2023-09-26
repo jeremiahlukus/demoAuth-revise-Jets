d9a29ac346d52c6902fd20bca5f8a6f1f1b35b42
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2023-09-26 19:56:59 -0400

    name change

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,13 +1,13 @@
 class AdminController < ReviseAuthController
   layout "admin_application"
   before_action :authenticate_admin
-  before_action :gravatar_url_for
+  before_action :gravatar_url
 
   def authenticate_admin
     redirect_to "/", alert: "Not authorized." unless current_user&.admin?
   end
 
-  def gravatar_url_for
+  def gravatar_url
     options = {}
     hash = Digest::MD5.hexdigest(current_user.email&.downcase || "")
     options.reverse_merge!(default: :mp, rating: :pg, size: 48)
